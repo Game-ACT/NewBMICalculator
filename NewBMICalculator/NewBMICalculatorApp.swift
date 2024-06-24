ebe7f36a8a3d85da3df3db3b7a4298a62593f9ab
noigamegun <76461912+noigamegun@users.noreply.github.com>
2024-06-24 13:39:26 +0700

    Initial Commit

---
+++ NewBMICalculator/NewBMICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewBMICalculatorApp.swift
+//  NewBMICalculator
+//
+//  Created by Thapat Auechaikasem on 24/6/67.
+//
+
+import SwiftUI
+
+@main
+struct NewBMICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
